76b61b8a9bb25268ac3032e878d9576bf8e63e00
accinn <aylinpt02@gmail.com>
2025-07-24 13:59:30 -0300

    update cv

--- codigo.js
+++ codigo.js
@@ -15,20 +15,16 @@ themeToggle.addEventListener("click", () => {
 document.addEventListener("DOMContentLoaded", function () {
     const languageSelect = document.getElementById("language-select");
 
-    // Cargar el idioma guardado
     const savedLanguage = localStorage.getItem("selectedLanguage");
 
-    // Si hay un idioma guardado, seleccionarlo en el dropdown
     if (savedLanguage) {
         languageSelect.value = savedLanguage;
     }
 
-    // Manejar el cambio de idioma
     languageSelect.addEventListener("change", function () {
         const selectedLanguage = this.value;
         localStorage.setItem("selectedLanguage", selectedLanguage); // Guardar idioma
 
-        // Redirigir según el idioma seleccionado
         if (selectedLanguage === "en" && window.location.pathname !== "/index-en.html") {
             window.location.href = "index-en.html";
         } else if (selectedLanguage === "es" && window.location.pathname !== "/index.html") {
@@ -39,25 +35,22 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 const hamburger = document.getElementById('hamburger');
-const nav = document.querySelector('.nav'); // Selecciona todo el contenedor nav
-const navItems = document.querySelectorAll('.nav-links li a'); // Selecciona los enlaces del menú
+const nav = document.querySelector('.nav'); 
+const navItems = document.querySelectorAll('.nav-links li a'); 
 
-// Alternar la visibilidad del menú hamburguesa
 hamburger.addEventListener('click', () => {
     event.stopPropagation();
-    nav.classList.toggle('show'); // Alterna la clase show en todo el nav
+    nav.classList.toggle('show'); 
 });
 
-// Cerrar el menú cuando se selecciona un elemento
 navItems.forEach(item => {
     item.addEventListener('click', () => {
-        nav.classList.remove('show'); // Oculta el menú después de seleccionar una opción
+        nav.classList.remove('show'); 
         const targetId = item.getAttribute('href');
         document.querySelector(targetId).scrollIntoView({ behavior: 'smooth' });
     });
 });
 
-// Cerrar el menú si se hace clic fuera de él
 document.addEventListener('click', (event) => {
     if (!nav.contains(event.target) && !hamburger.contains(event.target)) {
         nav.classList.remove('show');
@@ -83,7 +76,7 @@ document.getElementById('formContacto').addEventListener('submit', function(even
     .then(data => {
         document.getElementById('automensaje').textContent = "¡Gracias por comunicarte! Pronto te responderé.";
         document.getElementById('automensaje').style.color = "blue";
-        form.reset(); // Limpia el formulario después de enviar
+        form.reset(); 
     })
     .catch(error => {
         console.error('Error:', error);
